c878797ee331df983101e4b758267bf1d56a7a21
sallieeky <sallieeky@gmail.com>
2024-12-13 13:51:05 +0800

    ✨ (CategoryController.php, ProductController.php): introduce CategoryController and ProductController to manage categories and products 📝 (CategoryController.php, ProductController.php): add methods for listing and storing categories and products with appropriate request handling

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domains\Products\Actions\CreateProductAction;
+use App\Http\Requests\Products\GetProductsRequest;
+use App\Http\Requests\Products\StoreProductRequest;
+use App\Domains\Products\Models\Category;
+use App\Domains\Products\Models\Product;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends Controller
+{
+    public function index(GetProductsRequest $request)
+    {
+        $products = Product::search(
+            $request->getSearchTerm(),
+            $request->getSortBy(),
+            $request->getSortDirection(),
+        );
+
+        return [
+            'data' => $products->map->toData(),
+        ];
+    }
+
+    public function get(Product $product)
+    {
+        return ['data' => $product->toData()];
+    }
+
+    public function store(StoreProductRequest $request, CreateProductAction $createProduct)
+    {
+        $product = $createProduct->execute(
+            Category::findOrFail($request->getCategoryId()),
+            $request->getName(),
+            $request->getDescription(),
+            $request->getPrice()
+        );
+
+        return response([
+            'data' => $product
+        ], Response::HTTP_CREATED);
+    }
+}
