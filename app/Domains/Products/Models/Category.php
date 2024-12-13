90e035327c65609b11b4a62ef8446010d8399ca8
sallieeky <sallieeky@gmail.com>
2024-12-13 13:42:14 +0800

    ✨ feat(Product.php): add Product model to the Products domain 🐛 fix(Product.php): fix incorrect class name from User to Product in the Product model

--- app/Domains/Products/Models/Category.php
+++ app/Domains/Products/Models/Category.php
@@ -2,25 +2,19 @@
 
 namespace App\Domains\Products\Models;
 
-use Ecommerce\Common\DataTransferObjects\Product\CategoryData;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected static function newFactory()
-    {
-        return \Database\Factories\CategoryFactory::new();
-    }
+    // protected static function newFactory()
+    // {
+    //     return \Database\Factories\CategoryFactory::new();
+    // }
 
     public function products()
     {
         return $this->hasMany(Product::class);
-    }
-
-    public function toData(): CategoryData
-    {
-        return new CategoryData($this->id, $this->name);
     }
 }
