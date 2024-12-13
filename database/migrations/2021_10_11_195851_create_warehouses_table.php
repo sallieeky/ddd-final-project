d4ee604c9f07ecea84e8289e5efb86631d853062
sallieeky <sallieeky@gmail.com>
2024-12-13 14:11:05 +0800

    ✨ (migrations): create migrations for warehouses, products, and inventories tables to support inventory management functionality

---
+++ database/migrations/2021_10_11_195851_create_warehouses_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWarehousesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('warehouses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->timestamps();
+
+            $table->unique('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warehouses');
+    }
+}
---
+++ database/migrations/2021_10_12_175229_create_inventories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->nullable(false);
+            $table->foreignId('warehouse_id')->nullable(false);
+            $table->float('quantity')->nullable(false);
+            $table->timestamps();
+
+            $table->unique(['product_id', 'warehouse_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
